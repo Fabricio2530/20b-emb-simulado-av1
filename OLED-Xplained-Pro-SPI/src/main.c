d94d1e98b86ad36df36142c48df296f718be7911
Fabricio2530 <fabricio.neri.ismart@gmail.com>
2022-03-20 00:29:30 -0300

    subindo simulado

--- OLED-Xplained-Pro-SPI/src/main.c
+++ OLED-Xplained-Pro-SPI/src/main.c
@@ -1,24 +1,140 @@
 #include <asf.h>
 
+#include "PIO_OLED.h"
+#include "PIO_FUNCTIONS.h"
+#include "TC-RTT-RTC.h"
+
 #include "gfx_mono_ug_2832hsweg04.h"
 #include "gfx_mono_text.h"
 #include "sysfont.h"
+
+
+//Variaveis globais
+
+volatile char led1_ligado = 1;
+volatile char led2_ligado = 1;
+volatile char led3_ligado = 1;
+
+volatile char tc_flag1;
+volatile char tc_flag6;
+volatile char tc_flag3;
+
+
+void checa_tc_1(int flag){
+	
+	if (flag) {
+		tc_start(TC0, 1);
+	} else {
+		tc_stop(TC0, 1);
+	}
+	
+}
+
+void init(void) {
+	//Initialize the board clock
+	sysclk_init();
+	oled_init();
+	gfx_mono_ssd1306_init();
+	
+	// Desativa WatchDog Timer
+	WDT->WDT_MR = WDT_MR_WDDIS;
+}
+
+void TC1_Handler(void) {
+	/**
+	* Devemos indicar ao TC que a interrupção foi satisfeita.
+	* Isso é realizado pela leitura do status do periférico
+	**/
+	volatile uint32_t status = tc_get_status(TC0, 1);
+
+	/** Muda o estado do LED (pisca) **/
+	tc_flag1 = 1;
+}
+
+void TC3_Handler(void) {
+	/**
+	* Devemos indicar ao TC que a interrupção foi satisfeita.
+	* Isso é realizado pela leitura do status do periférico
+	**/
+	volatile uint32_t status = tc_get_status(TC1, 0);
+
+	/** Muda o estado do LED (pisca) **/
+	
+	tc_flag3 = 1;
+	
+}
+
+void TC6_Handler(void) {
+	/**
+	* Devemos indicar ao TC que a interrupção foi satisfeita.
+	* Isso é realizado pela leitura do status do periférico
+	**/
+	volatile uint32_t status = tc_get_status(TC2, 0);
+
+	/** Muda o estado do LED (pisca) **/
+	tc_flag6 = 1;
+	
+}
 
 int main (void)
 {
 	board_init();
 	sysclk_init();
+	init();
 	delay_init();
+	
+	TC_init(TC2, ID_TC6, 0, 1);
+	tc_start(TC2, 0);
+	
+	TC_init(TC1, ID_TC3, 0, 10);
+	tc_start(TC1, 0);
+	
+	TC_init(TC0, ID_TC1, 1, 5);
+	//tc_start(TC0, 1);
 
-  // Init OLED
-	gfx_mono_ssd1306_init();
-  
-  // Escreve na tela um circulo e um texto
-	gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
-  gfx_mono_draw_string("mundo", 50,16, &sysfont);
-
+	gfx_mono_draw_string("Aguardando", 0,16, &sysfont);
   /* Insert application code here, after the board has been initialized. */
 	while(1) {
-
+			
+			if (flag_but1) {
+				gfx_mono_draw_string("Botao 1   ", 0,16, &sysfont);
+				tc_stop(TC0, 1);
+				led1_ligado = !led1_ligado;
+				delay_ms(100);
+				checa_tc_1(led1_ligado);
+				flag_but1 = 0;
+			}
+				
+			if (flag_but2) {
+				gfx_mono_draw_string("Botao 2   ", 0,16, &sysfont);
+				led2_ligado = !led2_ligado;
+				flag_but2 = 0;
+			}
+			
+			if (flag_but3) {
+				gfx_mono_draw_string("Botao 3    ", 0,16, &sysfont);
+				led3_ligado = !led3_ligado;
+				flag_but3 = 0;
+			}
+			
+			
+			if (tc_flag1) {
+				pin_toggle(LED_PI1, LED_PI1_IDX_MASK);
+				tc_flag1 = 0;
+			}
+			
+			if (tc_flag3) {
+				pin_toggle(LED_PI2, LED_PI2_IDX_MASK);
+				tc_flag3 = 0;
+			}
+			
+			if (tc_flag6) {
+				
+				pin_toggle(LED_PI3, LED_PI3_IDX_MASK);
+				tc_flag6 = 0;
+			}
+			
+			pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
+			
 	}
 }
